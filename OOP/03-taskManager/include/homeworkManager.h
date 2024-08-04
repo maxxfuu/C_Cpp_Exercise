a446d37d2b14ae3ef58d47c8a1446905eebff1a1
Max Fu <mfu27@ucmerced.edu>
2024-08-03 23:13:24 -0700

    working on the rule of 5, move assignment and move constructor

---
+++ OOP/03-taskManager/include/homeworkManager.h
@@ -0,0 +1,18 @@
+#ifndef HOMEWORK_MANAGER 
+#define HOMEWORK_MANAGER 
+
+class homeworkManager { 
+
+    private: 
+
+    public: 
+
+}; 
+
+#endif 
+
+// Rule of 5 
+// Move Constructor and Move Assignment Operator. 
+
+// These are two options for clases to transfer ownership of a resource
+// from one object to another. 
--- OOP/03-taskManager/include/mathAssignment.h
+++ OOP/03-taskManager/include/mathAssignment.h
@@ -2,8 +2,22 @@
 #define MATH_ASSIGNMNET 
 #include "assignment.h" 
 class mathAssignment : public assignment {  
-    
-    public: 
+    private: 
+        std::string chapterName; 
+        int problemCount; 
+        std::chrono::system_clock::time_point dueDate;
 
+    public: 
+        mathAssignment(); 
+        mathAssignment(std::string name, int problem, std::chrono::system_clock::time_point dd);  
+
+    std::string getDetail() const override { 
+        return "Math " + chapterName + ", " + std::to_string(problemCount) + " Problems";   
+    } 
+
+    std::chrono::system_clock::time_point getDueDate() const override { 
+        return "Due Date: ", dueDate; 
+    }  
+ 
 }; 
 #endif
