43fb79c718ec1a32edd044008a2b1c88c25f70df
Max Fu <mfu27@ucmerced.edu>
2024-08-03 23:12:41 -0700

    new exercise

--- ComputerSystems/2_10.c
+++ ComputerSystems/2_10.c
@@ -28,10 +28,24 @@ void show_pointer(void *x) {
 }
 
 void inplace_swap(int *x, int *y) { 
+
+    *y = *x ^ *y; 
+    *x = *x ^ *y; 
+    *y = *x ^ *y; 
     
-}
+} 
+
+void reverse_array(int a[], int cnt) {
+    int first, last; 
+    for (first = 0, last - cnt -1, first < last, first++, last--) { 
+         inplace_swap(&a[first], &a[last]); 
+    }
+} 
+
 
 int main() { 
 
-}
+return 0; 
+
+} 
  
